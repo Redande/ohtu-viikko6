48db83b12896f0be6073fd01b5b376f116540480
Antti Leinonen <antti.leinonen@helsinki.fi>
2016-05-02 07:04:07 +0300

    1 tehtävän palautus

---
+++ viikko6/Laskin/src/ohtu/Erotus.java
@@ -0,0 +1,36 @@
+package ohtu;
+
+import javax.swing.JTextField;
+
+public class Erotus implements Komento {
+
+    private Sovelluslogiikka sovellus;
+    private JTextField tuloskentta;
+    private JTextField syotekentta;
+
+    public Erotus(Sovelluslogiikka sovellus, JTextField tuloskentta, JTextField syotekentta) {
+        this.sovellus = sovellus;
+        this.tuloskentta = tuloskentta;
+        this.syotekentta = syotekentta;
+    }
+
+    @Override
+    public void suorita() {
+        int arvo = 0;
+
+        try {
+            arvo = Integer.parseInt(syotekentta.getText());
+        } catch (Exception e) {
+        }
+
+        sovellus.miinus(arvo);
+
+        syotekentta.setText("");
+        tuloskentta.setText("" + sovellus.tulos());
+    }
+
+    @Override
+    public void peru() {
+    }
+
+}
